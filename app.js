ada3d440d13c7c6bc17776ef094a2a4c45aac874
David Redding <redding.david35@gmail.com>
2021-07-15 18:07:24 -0500

    add input

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const myHeading = document.querySelector('#myHeading');
 const myButton = document.querySelector('#myButton');
+const myInput = document.querySelector('#myTextInput');
 myButton.addEventListener('click', () => myHeading.style.color = 'red'); 
