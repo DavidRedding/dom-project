1aba49a7ba20d262fe9ee0e35a039f46374dd9c5
David Redding <redding.david35@gmail.com>
2021-07-16 06:48:10 -0500

    hide list

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let listDescription = document.querySelector('p.description');
 let descriptionButton = document.querySelector('button.description');
 let desctiptionInput = document.querySelector('input.description');
+let hideButton = document.querySelectorAll('button')[0];
+let ul = document.querySelector('UL');
 
 descriptionButton.addEventListener('click', () => {
     listDescription.textContent = `Things that are ${desctiptionInput.value}:`
 })
+
+hideButton.addEventListener('click', () => ul.style.display = 'none');
