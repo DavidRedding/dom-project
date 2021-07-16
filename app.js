67dc1e24a71d0ece2b9b93d82bc0568b09509a5d
David Redding <redding.david35@gmail.com>
2021-07-16 07:23:31 -0500

    replace ul with listDiv, organized js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-let listDescription = document.querySelector('p.description');
-let descriptionButton = document.querySelector('button.description');
-let desctiptionInput = document.querySelector('input.description');
-let hideButton = document.querySelectorAll('button')[0];
-let ul = document.querySelector('UL');
+let hideButton = document.querySelectorAll("button")[0];
+let listDiv = document.querySelector(".listDiv");
+let listDescription = document.querySelector("p.description");
+let descriptionButton = document.querySelector("button.description");
+let desctiptionInput = document.querySelector("input.description");
 
-descriptionButton.addEventListener('click', () => {
-    listDescription.textContent = `Things that are ${desctiptionInput.value}:`
-})
+hideButton.addEventListener("click", () => {
+  if (listDiv.style.display != "none") {
+    listDiv.style.display = "none";
+    hideButton.textContent = `Show List`;
+  } else{
+      listDiv.style.display = 'block';
+      hideButton.textContent = `Hide List`;
+    }
+});
 
-hideButton.addEventListener('click', () => ul.style.display = 'none');
+descriptionButton.addEventListener("click", () => {
+  listDescription.textContent = `Things that are ${desctiptionInput.value}:`;
+});
