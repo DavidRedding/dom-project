d833cc8c95b02592859c3f80822520fb09014a1b
David Redding <redding.david35@gmail.com>
2021-07-16 08:44:19 -0500

    fixed description bug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let hideButton = document.querySelectorAll("button")[0];
 let listDiv = document.querySelector(".listDiv");
 let listDescription = document.querySelector("p.description");
 let descriptionButton = document.querySelector("button.description");
-let desctiptionInput = document.querySelector("input.description");
+let descriptionInput = document.querySelector("input.description");
 let ul = document.querySelector('ul');
 let addItemInput = document.querySelector("input.addItem");
 let addItemButton = document.querySelector("button.addItem");
@@ -19,7 +19,8 @@ hideButton.addEventListener("click", () => {
 });
 
 descriptionButton.addEventListener("click", () => {
-  listDescription.textContent = `Things that are ${desctiptionInput.value}:`;
+  listDescription.textContent = `Things that are ${descriptionInput.value}:`;
+  descriptionInput.value = '';
 });
 
 addItemButton.addEventListener('click', () => {
@@ -33,3 +34,5 @@ removeItem.addEventListener('click', () => {
     let li = document.querySelector('li:last-child');
     ul.removeChild(li); 
 })
+
+
