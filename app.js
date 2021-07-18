cca0de056ffe8818cdbe5a7059ccc37a6883a2e9
David Redding <redding.david35@gmail.com>
2021-07-18 11:45:18 -0500

    deleted buttons html, added new item buttons

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ let descriptionButton = document.querySelector("button.description");
 let descriptionInput = document.querySelector("input.description");
 let addItemInput = document.querySelector("input.addItem");
 let addItemButton = document.querySelector("button.addItem");
+let ul = listDiv.children[3];
+
+let addButtons = ul => {        //ul = ul.children
+    for (let i = 0; i < ul.children.length; i++) {
+      ul.children[i].innerHTML += `
+      <button id="up">Up</button>
+      <button id='remove'>Remove</button>
+      <button id='down'>Down</button>
+      `;
+}}
+
+addButtons(ul);
+
+let addSingleButton = (li) => {
+    li.innerHTML += `
+    <button id="up">Up</button>
+    <button id='remove'>Remove</button>
+    <button id='down'>Down</button>
+    `;
+}
 
 hideButton.addEventListener("click", () => {
   if (listDiv.style.display != "none") {
@@ -25,16 +45,19 @@ addItemButton.addEventListener('click', () => {
     let li = document.createElement('LI');
     li.textContent = addItemInput.value;
     ul.appendChild(li);
+    addSingleButton(li);
     addItemInput.value = '';
 })
 
-let ul = listDiv.children[3];
+
 ul.addEventListener('click', (e) => {
     if (e.target.tagName == ('BUTTON')){
+
         if (e.target.id == 'remove') {
             let li = e.target.parentNode;
             ul.removeChild(li);   
         }
+        
         if (e.target.id == 'up') {
             let li = e.target.parentNode;
             let prevLi = li.previousElementSibling;
@@ -43,13 +66,17 @@ ul.addEventListener('click', (e) => {
               ul.insertBefore(li, prevLi); // puts li on top of prevLi
             } 
         }
+
         if (e.target.id == 'down') {
             let li = e.target.parentNode;
             let ul = li.parentNode;
             let nextLi = li.nextElementSibling;
-            if(nextEl){ // checks null
+            if(nextLi){ // checks null
                 ul.insertBefore(nextLi,li); // sits nextLi on top of li
             }
         }
     }
 })
+
+                                
+
